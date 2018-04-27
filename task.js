0afc7142b36b6c9e27645ccfed252a50cfa891a5
8mhampe <31480929+8mhampe@users.noreply.github.com>
2018-04-27 09:16:51 -0500

    Add files via upload

---
+++ task.js
@@ -0,0 +1,31 @@
+//The code at the bottom is used to export this file as a module to be used in the client file.
+//The fist line sets up my export as well, ignore it.
+const TaskExport = function(){
+
+    //returns a task object, similar to pizza
+    function createTask(v,t){
+        return{
+            value:v;
+            time:t;
+        }
+    }
+
+    //returns a random integer between 0 and n inclusive
+    function rInt(n){
+        return  Math.floor( n * Math.random());
+    }
+    //returns a list of task objects.
+    //You must reference the function above to create them each time
+    //make all times range from 0 to 15 and all values range from 0 to 100
+    function Factory(n){
+        let f1 = new createTask(rInt(100),rInt(15));
+    }
+
+    //do not change anything below there!
+    return{
+        createTask,
+        Factory
+    }
+
+}
+module.exports = TaskExport;
